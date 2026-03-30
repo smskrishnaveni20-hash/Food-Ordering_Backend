01898a9e61d31aab3e2a1dc83ceb20d60d507b8f
Educaddnta24 <aanianandeducaddnta@gmail.com>
2026-03-30 15:59:30 +0530

    updated

--- Food_Ordering/asgi.py
+++ Food_Ordering/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-settings_module = 'Food_Ordering.deployement_settings' if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "Food_Ordering.settings"
+settings_module = 'Food_Ordering.deployment_settings' if "RENDER_EXTERNAL_HOSTNAME" in os.environ else "Food_Ordering.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
