fa6b22c39168d8531b1def6efd5304b0f2161e55
smskrishnaveni20-hash <smskrishnaveni20@gmail.com>
2026-06-27 20:18:38 +0530

    Update urls.py

--- Food_Ordering/urls.py
+++ Food_Ordering/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import staticfrom django.http import JsonResponse
+
+def root(request):
+    return JsonResponse({"message": "Food Ordering API is running!", "status": "ok"})
 
 
 urlpatterns = [
+    path('', root),
     path('admin/', admin.site.urls),
     path('', include('API.urls')),
 
