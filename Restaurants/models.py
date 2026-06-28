b14670499f82896bae70ccb4920926b17eb6cc50
smskrishnaveni20-hash <smskrishnaveni20@gmail.com>
2026-06-28 19:30:00 +0530

    Update models.py

--- Restaurants/models.py
+++ Restaurants/models.py
@@ -7,7 +7,7 @@ class Restaurants(models.Model):
     Images = models.ImageField(upload_to="media",null=True, blank = True)
     Restaurant_Name = models.CharField(max_length=35)
     Restaurant_Location = models.TextField(max_length=250)
-    Mobile_Number = PhoneNumberField(max_length=13, default="000 000 0000")
+    Mobile_Number = PhoneNumberField(max_length=20, default="000 000 0000")
     Url_Offical = models.URLField(max_length=200, blank= True, null= True)
     Rating = models.DecimalField(max_digits=5, decimal_places=1)
 
